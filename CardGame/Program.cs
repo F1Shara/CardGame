909e15d4703122dd4a71c49244d8c5345e55aa7d
Myroslav Snigur <msnigur.vn@gmail.com>
2020-11-05 20:06:21 +0200

    Add Program.cs

---
+++ CardGame/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using GameLibrary;
+
+namespace CardGame
+{
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+            GameController controller = new GameController();
+            
+            controller.StartGame();            
+            controller.Play();
+
+        }
+
+
+    }
+}
